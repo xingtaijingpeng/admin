d675026dfcbecf181bd6f625c4090070439178a0
duanzhiwei <89340545@qq.com>
2020-04-05 22:24:53 +0800

    'bak'

--- app/Packages/Article/Models/Article.php
+++ app/Packages/Article/Models/Article.php
@@ -9,7 +9,15 @@
 namespace App\Packages\Article\Models;
 
 
+use App\Packages\Category\Model\Category;
+
 class Article
 {
-
+    /**
+     * 获取指定文章所有分类
+     */
+    public function categories()
+    {
+        return $this->morphToMany(Category::class, 'categoryable');
+    }
 }
---
+++ database/migrations/2020_04_05_141155_create_category_tables.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('guard')->default('')->comment('守卫');
+            $table->string('name')->default('')->comment('');
+            $table->Integer('parent_id')->default(0)->comment('');
+            $table->tinyInteger('status')->default(1)->comment('1正常 2停止');
+            $table->timestamps();
+        });
+        Schema::create('categoryables', function (Blueprint $table) {
+            $table->Integer('category_id')->default(0)->comment('');
+            $table->string('model_type')->default('')->comment('');
+            $table->Integer('model_id')->default(0)->comment('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('categoryables');
+    }
+}
